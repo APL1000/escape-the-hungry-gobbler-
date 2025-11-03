186fc4cef9385950dc48281d2974aa54bdd332de
Echo-Path-Coder <Echopath2023@outlook.com>
2025-11-02 22:21:53 -0600

    Update screens.js

--- screens.js
+++ screens.js
@@ -473,10 +473,8 @@ function updateLeaderboard(initials, location, score) {
 function generateDailyLeaderboard() {
   //Check today's date
 
-  localStorage.removeItem("leaderboard");
-  localStorage.removeItem("leaderboardDate");
-  generateDailyLeaderboard();
-  
+  localStorage.removeItem("gobblerleaderboard");
+  localStorage.removeItem("gobblerleaderboardDate");
   
   
   const today = new Date().toISOString().split("T")[0];
