dbc50f714e81dd8005573108de62be4a946661c6
APL1000 <andylax149@gmail.com>
2025-11-02 22:33:48 -0500

    Clean up localStorage by removing old leaderboard data
    Remove outdated leaderboard items from localStorage.

--- screens.js
+++ screens.js
@@ -469,7 +469,9 @@ function updateLeaderboard(initials, location, score) {
   // Save updated leaderboard back to localStorage
   localStorage.setItem("gobblerleaderboard", JSON.stringify(leaderboard));
 }
-
+localStorage.removeItem("leaderboard");
+  localStorage.removeItem("leaderboardDate");
+  generateDailyLeaderboard();
 function generateDailyLeaderboard() {
   //Check today's date
   const today = new Date().toISOString().split("T")[0];
